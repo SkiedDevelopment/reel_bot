b787e2204578da1cfd9e201b30aa867b04d7dbaa
Lume1530 <dasilva.lume@gmail.com>
2025-04-27 14:01:40 +0530

    Update reel_tracker_bot.py

--- reel_tracker_bot.py
+++ reel_tracker_bot.py
@@ -330,7 +330,7 @@ async def broadcast(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 app = Application.builder().token(TOKEN).build()
 
-app.add_handler(CommandHandler("start", start_command))
+app.add_handler(CommandHandler("start", start_cmd))
 app.add_handler(CommandHandler("add", add_reel))
 app.add_handler(CommandHandler("myreels", my_reels))
 app.add_handler(CommandHandler("forceupdate", force_update))
